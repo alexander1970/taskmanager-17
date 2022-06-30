f721ec16da962062e8ba28d92a40ff7dac3b7f86
alexander1970 <alexander_shloma@mail.ru>
2022-07-01 01:06:12 +0300

    3.1.3 Реализует смену карточки на форму

--- src/presenter/board-presenter.js
+++ src/presenter/board-presenter.js
@@ -7,23 +7,51 @@ import LoadMoreButtonView from '../view/load-more-button-view.js';
 import {render} from '../render.js';
 
 export default class BoardPresenter {
-  boardComponent = new BoardView();
-  taskListComponent = new TaskListView();
+  #boardContainer = null;
+  #tasksModel = null;
+
+  #boardComponent = new BoardView();
+  #taskListComponent = new TaskListView();
+
+  #boardTasks = [];
 
   init = (boardContainer, tasksModel) => {
-    this.boardContainer = boardContainer;
-    this.tasksModel = tasksModel;
-    this.boardTasks = [...this.tasksModel.getTasks()];
+    this.#boardContainer = boardContainer;
+    this.#tasksModel = tasksModel;
+    this.#boardTasks = [...this.#tasksModel.tasks];
 
-    render(this.boardComponent, this.boardContainer);
-    render(new SortView(), this.boardComponent.getElement());
-    render(this.taskListComponent, this.boardComponent.getElement());
-    render(new TaskEditView(this.boardTasks[0]), this.taskListComponent.getElement());
+    render(this.#boardComponent, this.#boardContainer);
+    render(new SortView(), this.#boardComponent.element);
+    render(this.#taskListComponent, this.#boardComponent.element);
 
-    for (let i = 1; i < this.boardTasks.length; i++) {
-      render(new TaskView(this.boardTasks[i]), this.taskListComponent.getElement());
+    for (let i = 0; i < this.#boardTasks.length; i++) {
+      this.#renderTask(this.#boardTasks[i]);
     }
 
-    render(new LoadMoreButtonView(), this.boardComponent.getElement());
+    render(new LoadMoreButtonView(), this.#boardComponent.element);
+  };
+
+  #renderTask = (task) => {
+    const taskComponent = new TaskView(task);
+    const taskEditComponent = new TaskEditView(task);
+
+    const replaceCardToForm = () => {
+      this.#taskListComponent.element.replaceChild(taskEditComponent.element, taskComponent.element);
+    };
+
+    const replaceFormToCard = () => {
+      this.#taskListComponent.element.replaceChild(taskComponent.element, taskEditComponent.element);
+    };
+
+    taskComponent.element.querySelector('.card__btn--edit').addEventListener('click', () => {
+      replaceCardToForm();
+    });
+
+    taskEditComponent.element.querySelector('form').addEventListener('submit', (evt) => {
+      evt.preventDefault();
+      replaceFormToCard();
+    });
+
+    render(taskComponent, this.#taskListComponent.element);
   };
 }
