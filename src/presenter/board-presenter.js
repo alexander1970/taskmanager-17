2cd8b7b7da02873b59e3e07ad9c90ab0b8b5106b
alexander1970 <alexander_shloma@mail.ru>
2022-07-01 15:36:22 +0300

    3.2.2 Добавит логику допоказа задач

--- src/presenter/board-presenter.js
+++ src/presenter/board-presenter.js
@@ -6,14 +6,18 @@ import TaskEditView from '../view/task-edit-view.js';
 import LoadMoreButtonView from '../view/load-more-button-view.js';
 import {render} from '../render.js';
 
+const TASK_COUNT_PER_STEP = 8;
+
 export default class BoardPresenter {
   #boardContainer = null;
   #tasksModel = null;
 
   #boardComponent = new BoardView();
   #taskListComponent = new TaskListView();
+  #loadMoreButtonComponent = new LoadMoreButtonView();
 
   #boardTasks = [];
+  #renderedTaskCount = TASK_COUNT_PER_STEP;
 
   init = (boardContainer, tasksModel) => {
     this.#boardContainer = boardContainer;
@@ -24,11 +28,29 @@ export default class BoardPresenter {
     render(new SortView(), this.#boardComponent.element);
     render(this.#taskListComponent, this.#boardComponent.element);
 
-    for (let i = 0; i < this.#boardTasks.length; i++) {
+    for (let i = 0; i < Math.min(this.#boardTasks.length, TASK_COUNT_PER_STEP); i++) {
       this.#renderTask(this.#boardTasks[i]);
     }
 
-    render(new LoadMoreButtonView(), this.#boardComponent.element);
+    if (this.#boardTasks.length > TASK_COUNT_PER_STEP) {
+      render(this.#loadMoreButtonComponent, this.#boardComponent.element);
+
+      this.#loadMoreButtonComponent.element.addEventListener('click', this.#handleLoadMoreButtonClick);
+    }
+  };
+
+  #handleLoadMoreButtonClick = (evt) => {
+    evt.preventDefault();
+    this.#boardTasks
+      .slice(this.#renderedTaskCount, this.#renderedTaskCount + TASK_COUNT_PER_STEP)
+      .forEach((task) => this.#renderTask(task));
+
+    this.#renderedTaskCount += TASK_COUNT_PER_STEP;
+
+    if (this.#renderedTaskCount >= this.#boardTasks.length) {
+      this.#loadMoreButtonComponent.element.remove();
+      this.#loadMoreButtonComponent.removeElement();
+    }
   };
 
   #renderTask = (task) => {
@@ -43,13 +65,23 @@ export default class BoardPresenter {
       this.#taskListComponent.element.replaceChild(taskComponent.element, taskEditComponent.element);
     };
 
+    const onEscKeyDown = (evt) => {
+      if (evt.key === 'Escape' || evt.key === 'Esc') {
+        evt.preventDefault();
+        replaceFormToCard();
+        document.removeEventListener('keydown', onEscKeyDown);
+      }
+    };
+
     taskComponent.element.querySelector('.card__btn--edit').addEventListener('click', () => {
       replaceCardToForm();
+      document.addEventListener('keydown', onEscKeyDown);
     });
 
     taskEditComponent.element.querySelector('form').addEventListener('submit', (evt) => {
       evt.preventDefault();
       replaceFormToCard();
+      document.removeEventListener('keydown', onEscKeyDown);
     });
 
     render(taskComponent, this.#taskListComponent.element);
