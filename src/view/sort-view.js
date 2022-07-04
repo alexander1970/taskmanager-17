101a5d082735039ed6bef95699176ab1e5ac9b34
alexander1970 <alexander_shloma@mail.ru>
2022-07-04 22:16:26 +0300

    5.2.3 Опишет логику сортировки. WIP

--- src/view/sort-view.js
+++ src/view/sort-view.js
@@ -1,10 +1,11 @@
+import { SortType } from '../const';
 import AbstractView from '../framework/view/abstract-view';
 
 const createSortTemplate = () => (
   `<div class="board__sort-list">
-    <a href="#" class="board__sort-item">СОРТИРОВАТЬ ПО УМОЛЧАНИЮ</a>
-    <a href="#" class="board__sort-item">СОРТИРОВАТЬ ПО ДАТЕ вверх</a>
-    <a href="#" class="board__sort-item">СОРТИРОВАТЬ ПО ДАТЕ вниз</a>
+    <a href="#" class="board__sort-item" date-sort-type${SortType.DEFAULT}>СОРТИРОВАТЬ ПО УМОЛЧАНИЮ</a>
+    <a href="#" class="board__sort-item" date-sort-type${SortType.DATE_UP}>СОРТИРОВАТЬ ПО ВОЗВРАСТАНИЮ</a>
+    <a href="#" class="board__sort-item" date-sort-type${SortType.DATE_DOWN}>СОРТИРОВАТЬ ПО УБЫВАНИЮ</a>
   </div>`
 );
 
@@ -12,4 +13,18 @@ export default class SortView extends AbstractView {
   get template() {
     return createSortTemplate();
   }
+
+  setSortTypeChangeHandler = (callback) => {
+    this._callback.sortTypeChange = callback;
+    this.element.addEventListener('click', this.#sortTypeChangeHandler);
+  };
+
+  #sortTypeChangeHandler = (evt) => {
+    if (evt.target.tagName !== 'A') {
+      return;
+    }
+
+    evt.preventDefault();
+    this._callback.sortTypeChange(evt.target.dataset.sortType);
+  };
 }
